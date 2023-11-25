eeb5d01f281da062d8518341dce105e90a1e529e
dev-leomadeira <dev.leomadeira@gmail.com>
2023-11-25 00:07:07 -0300

    commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App(){
+  return(
+    <div className="App">
+      <h1>To-do List</h1>
+      {/* components todo list */}
+    </div>
+  );
+}
+
+export default App;
